aed0aa7fd2e60f92871dda47161bf4fbf8b6729a
remagss <slprtzd@gmail.com>
2025-11-23 23:35:51 +0300

    feat: add new function process_file_with_flags()

--- src/cat/s21_cat_output.c
+++ src/cat/s21_cat_output.c
@@ -1,1 +1,15 @@
 #include "s21_cat_output.h"
+
+void process_file_with_flags(FILE *file, cat_flags *flags) {
+    int ch;
+    int line_number = 1; // for line numbering (-n, -b)
+    int prev_ch = '\n'; // for detect new line
+    int empty_line_count = 0; // (-s)
+    
+    while ((ch = fgetc(file)) != EOF) {
+        int should_print = process_character(ch, flags, &line_number, &prev_ch, &empty_line_count);
+        if (should_print) {
+            putchar(ch);
+        }
+    }
+}
