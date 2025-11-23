8730048acff90c3c1aa0f1b782c8201597fd5be0
remagss <slprtzd@gmail.com>
2025-11-23 23:44:05 +0300

    feat: add new funtions for flags (-s, -v, -e, -t, -E, -T)

--- src/cat/s21_cat_output.c
+++ src/cat/s21_cat_output.c
@@ -38,3 +38,45 @@ void handle_numbering(cat_flags *flags, int ch, int *line_number, int *prev_ch) 
         printf("%6d\t", (*line_number)++);
     }
 }
+
+int handle_squeeze_blank(cat_flags *flags, int ch, int *prev_ch, int *empty_line_count) {
+    int should_print = 1;
+    
+    if (flags->squeeze_blank) {
+        if (ch == '\n' && *prev_ch == '\n') {
+            (*empty_line_count)++;
+            if (*empty_line_count > 1) {
+                should_print = 0;
+            }
+        } else {
+            *empty_line_count = 0;
+        }
+    }
+    
+    return should_print;
+}
+
+int handle_nonprinting(int ch, cat_flags *flags) {
+    if (flags->show_nonprinting || flags->show_ends || flags->show_tabs) {
+        if (ch == '\n' && flags->show_ends) {
+            printf("$");
+        }
+        
+        if (ch == '\t' && flags->show_tabs) {
+            printf("^I");
+            return -1;
+        }
+        
+        if (flags->show_nonprinting) {
+            if (ch < 32 && ch != 9 && ch != 10) {
+                printf("^%c", ch + 64);
+                return -1;
+            } else if (ch == 127) {
+                printf("^?");
+                return -1;
+            }
+        }
+    }
+    
+    return ch;
+}
