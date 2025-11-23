5813d3a9e8634d1397d7793f5b553c7a1f450fe5
remagss <slprtzd@gmail.com>
2025-11-23 23:29:43 +0300

    feat: create new files for realization output function with flags

---
+++ src/cat/s21_cat_output.h
@@ -0,0 +1,12 @@
+#ifndef S21_CAT_OUTPUT_H
+#define S21_CAT_OUTPUT_H
+
+#include "s21_cat.h"
+
+void process_file_with_flags(FILE *file, cat_flags *flags);
+int process_character(int ch, cat_flags *flags, int *line_number, int *prev_ch, int *empty_line_count);
+void handle_numbering(cat_flags *flags, int ch, int *line_number, int *prev_ch);
+int handle_squeeze_blank(cat_flags *flags, int ch, int *prev_ch, int *empty_line_count);
+int handle_nonprinting(int ch, cat_flags *flags);
+
+#endif
