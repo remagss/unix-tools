49b6fa6041174c7f6530bbb9c290fbe295c0f14e
remagss <slprtzd@gmail.com>
2025-11-24 00:44:15 +0300

    fix: change logic for pass tests

--- src/cat/s21_cat_flags.c
+++ src/cat/s21_cat_flags.c
@@ -2,31 +2,35 @@
 
 void process_file_with_flags(FILE *file, cat_flags *flags) {
     int ch;
-    int line_number = 1; // for line numbering (-n, -b)
-    int prev_ch = '\n'; // for detect new line
-    int empty_line_count = 0; // (-s)
+    int line_number = 1;
+    int prev_ch = '\n';
+    int empty_line_count = 0;
     
     while ((ch = fgetc(file)) != EOF) {
-        int should_print = process_character(ch, flags, &line_number, &prev_ch, &empty_line_count);
-        if (should_print) {
-            putchar(ch);
+        int result = process_character(ch, flags, &line_number, &prev_ch, &empty_line_count);
+        if (result != -1) {
+            putchar(result);
         }
     }
 }
 
 int process_character(int ch, cat_flags *flags, int *line_number, int *prev_ch, int *empty_line_count) {
     int should_print = 1;
+    int processed_char = ch;
     
     handle_numbering(flags, ch, line_number, prev_ch);
     
     should_print = handle_squeeze_blank(flags, ch, prev_ch, empty_line_count);
     
     if (should_print) {
-        ch = handle_nonprinting(ch, flags);
+        processed_char = handle_nonprinting(ch, flags);
+        if (processed_char == -1) {
+            should_print = 0;
+        }
     }
     
     *prev_ch = ch;
-    return should_print;
+    return should_print ? processed_char : -1;
 }
 
 void handle_numbering(cat_flags *flags, int ch, int *line_number, int *prev_ch) {
@@ -57,24 +61,28 @@ int handle_squeeze_blank(cat_flags *flags, int ch, int *prev_ch, int *empty_line
 }
 
 int handle_nonprinting(int ch, cat_flags *flags) {
-    if (flags->show_nonprinting || flags->show_ends || flags->show_tabs) {
-        if (ch == '\n' && flags->show_ends) {
-            printf("$");
-        }
-        
-        if (ch == '\t' && flags->show_tabs) {
-            printf("^I");
+    if (ch == EOF) {
+        return ch;
+    }
+    
+    unsigned char uc = (unsigned char)ch;
+    
+    if (uc == '\n' && flags->show_ends) {
+        printf("$");
+    }
+    
+    if (uc == '\t' && flags->show_tabs) {
+        printf("^I");
+        return -1;
+    }
+    
+    if (flags->show_nonprinting) {
+        if (uc < 32 && uc != 9 && uc != 10) {
+            printf("^%c", uc + 64);
             return -1;
-        }
-        
-        if (flags->show_nonprinting) {
-            if (ch < 32 && ch != 9 && ch != 10) {
-                printf("^%c", ch + 64);
-                return -1;
-            } else if (ch == 127) {
-                printf("^?");
-                return -1;
-            }
+        } else if (uc == 127) {
+            printf("^?");
+            return -1;
         }
     }
     
