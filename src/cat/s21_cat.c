68b26cae91dc394f2d1f0721b9335f6e40e31669
remagss <slprtzd@gmail.com>
2025-11-24 01:06:33 +0300

    style: use clang-format

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,179 +1,184 @@
 #include "s21_cat.h"
+
 #include "s21_cat_flags.h"
 
 int main(int argc, char *argv[]) {
-    cat_flags flags = {0};
-    int exit_code = SUCCESS;
-    int should_process_files = 1;
+  cat_flags flags = {0};
+  int exit_code = SUCCESS;
+  int should_process_files = 1;
 
-    int run_flag = run_cat(argc, argv, &flags);
+  int run_flag = run_cat(argc, argv, &flags);
 
-    if (flags.show_help) {
-        show_help();
-        should_process_files = 0;
+  if (flags.show_help) {
+    show_help();
+    should_process_files = 0;
+  } else if (run_flag != SUCCESS) {
+    handle_error(run_flag, argv);
+    exit_code = run_flag;
+    should_process_files = 0;
+  }
+
+  if (should_process_files) {
+    for (int i = 1; i < argc; i++) {
+      if (argv[i][0] != '-') {
+        print_file_content(argv[i], &flags);
+      }
     }
-    else if (run_flag != SUCCESS) {
-        handle_error(run_flag, argv);
-        exit_code = run_flag;
-        should_process_files = 0;
-    }
+  }
 
-    if (should_process_files) {
-        for (int i = 1; i < argc; i++) {
-            if (argv[i][0] != '-') {
-                print_file_content(argv[i], &flags);
-            }
-        }
-    }
-
-    return exit_code;
+  return exit_code;
 }
 
 int run_cat(int argc, char *argv[], cat_flags *flags) {
-    int exit_code = SUCCESS;
+  int exit_code = SUCCESS;
 
-    if (argc < 2) {
-        exit_code = ERROR_INVALID_ARGS;
-    } else {
-        exit_code = parse_flags(argc, argv, flags);
-    }
-    return exit_code;
+  if (argc < 2) {
+    exit_code = ERROR_INVALID_ARGS;
+  } else {
+    exit_code = parse_flags(argc, argv, flags);
+  }
+  return exit_code;
 }
 
 int parse_flags(int argc, char *argv[], cat_flags *flags) {
-    int status_flag = SUCCESS;
+  int status_flag = SUCCESS;
 
-    for (int i = 1; i < argc && status_flag == SUCCESS; i++) { 
-        if (argv[i][0] != '-') continue;
+  for (int i = 1; i < argc && status_flag == SUCCESS; i++) {
+    if (argv[i][0] != '-') continue;
 
-        if (argv[i][1] == '-') {
-            if (parse_gnu_flags(argv[i], flags) != SUCCESS) {
-                status_flag = ERROR_INVALID_GNU_FLAG;
-            }
-        } else {
-            if (parse_short_flags(argv[i], flags) != SUCCESS) {
-                status_flag = ERROR_INVALID_SHORT_FLAG;
-            }
-        }
+    if (argv[i][1] == '-') {
+      if (parse_gnu_flags(argv[i], flags) != SUCCESS) {
+        status_flag = ERROR_INVALID_GNU_FLAG;
+      }
+    } else {
+      if (parse_short_flags(argv[i], flags) != SUCCESS) {
+        status_flag = ERROR_INVALID_SHORT_FLAG;
+      }
     }
-    
-    if (flags->number_nonblank) {
-        flags->number = 0; // Disabled flag -n if -b is active 
-    }
+  }
 
-    return status_flag;
+  if (flags->number_nonblank) {
+    flags->number = 0;  // Disabled flag -n if -b is active
+  }
+
+  return status_flag;
 }
 
 int parse_gnu_flags(char *arg, cat_flags *flags) {
-    int status_flag = SUCCESS;
-    if (strcmp(arg, "--number-nonblank") == 0) {
-        flags->number_nonblank = 1;
-    } else if (strcmp(arg, "--number") == 0) {
-        flags->number = 1;
-    } else if (strcmp(arg, "--squeeze-blank") == 0) {
-        flags->squeeze_blank = 1;
-    } else if (strcmp(arg, "--help") == 0) {
-        flags->show_help = 1;
-    } else {
-        status_flag = ERROR; 
-    }
-    
-    return status_flag;
+  int status_flag = SUCCESS;
+  if (strcmp(arg, "--number-nonblank") == 0) {
+    flags->number_nonblank = 1;
+  } else if (strcmp(arg, "--number") == 0) {
+    flags->number = 1;
+  } else if (strcmp(arg, "--squeeze-blank") == 0) {
+    flags->squeeze_blank = 1;
+  } else if (strcmp(arg, "--help") == 0) {
+    flags->show_help = 1;
+  } else {
+    status_flag = ERROR;
+  }
+
+  return status_flag;
 }
 
 int parse_short_flags(char *arg, cat_flags *flags) {
-    int status_flag = SUCCESS;
+  int status_flag = SUCCESS;
 
-    for (int j = 1; arg[j] != '\0'; j++) {
-        switch (arg[j]) {
-            case 'b': 
-                flags->number_nonblank = 1; 
-                break;
-            case 'e': 
-                flags->show_ends = 1;
-                flags->show_nonprinting = 1; // implies -v
-                break;
-            case 'n': 
-                flags->number = 1;
-                break;
-            case 's':
-                flags->squeeze_blank = 1;
-                break;
-            case 't':
-                flags->show_tabs = 1;
-                flags->show_nonprinting = 1; // implies -v
-                break;
-            case 'v':
-                flags->show_nonprinting = 1;
-                break;
-            
-            case 'E':
-                flags->show_ends = 1;
-                break;
-            case 'T':
-                flags->show_tabs = 1;
-                break;
-            default:
-                status_flag = ERROR;
-        }
+  for (int j = 1; arg[j] != '\0'; j++) {
+    switch (arg[j]) {
+      case 'b':
+        flags->number_nonblank = 1;
+        break;
+      case 'e':
+        flags->show_ends = 1;
+        flags->show_nonprinting = 1;  // implies -v
+        break;
+      case 'n':
+        flags->number = 1;
+        break;
+      case 's':
+        flags->squeeze_blank = 1;
+        break;
+      case 't':
+        flags->show_tabs = 1;
+        flags->show_nonprinting = 1;  // implies -v
+        break;
+      case 'v':
+        flags->show_nonprinting = 1;
+        break;
+
+      case 'E':
+        flags->show_ends = 1;
+        break;
+      case 'T':
+        flags->show_tabs = 1;
+        break;
+      default:
+        status_flag = ERROR;
     }
+  }
 
-    return status_flag;
+  return status_flag;
 }
 
 void handle_error(int error_code, char *argv[]) {
-    switch(error_code) {
-        case ERROR:
-            fprintf(stderr, "%s: unknown error\n", argv[0]);
-            break;
-        case ERROR_INVALID_ARGS:
-            fprintf(stderr, "%s: invalid arguments\n", argv[0]);
-            break;
-        case ERROR_INVALID_SHORT_FLAG:
-            fprintf(stderr, "%s: invalid option\n", argv[0]);
-            break;
-        case ERROR_INVALID_GNU_FLAG:
-            fprintf(stderr, "%s: invalid GNU option\n", argv[0]);
-            break;
-        case ERROR_FILE_OPEN:
-            fprintf(stderr, "%s: No such file or directory\n", argv[0]);
-            break;
-    }
-    printf("Try './s21_cat --help' for more information.\n");
+  switch (error_code) {
+    case ERROR:
+      fprintf(stderr, "%s: unknown error\n", argv[0]);
+      break;
+    case ERROR_INVALID_ARGS:
+      fprintf(stderr, "%s: invalid arguments\n", argv[0]);
+      break;
+    case ERROR_INVALID_SHORT_FLAG:
+      fprintf(stderr, "%s: invalid option\n", argv[0]);
+      break;
+    case ERROR_INVALID_GNU_FLAG:
+      fprintf(stderr, "%s: invalid GNU option\n", argv[0]);
+      break;
+    case ERROR_FILE_OPEN:
+      fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+      break;
+  }
+  printf("Try './s21_cat --help' for more information.\n");
 }
 
 void print_file_content(const char *filename, cat_flags *flags) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        fprintf(stderr, "s21_cat: %s: No such file or directory\n", filename);
-        return;
-    }
-    
-    process_file_with_flags(file, flags);
-    
-    fclose(file);
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    fprintf(stderr, "s21_cat: %s: No such file or directory\n", filename);
+    return;
+  }
+
+  process_file_with_flags(file, flags);
+
+  fclose(file);
 }
 
 void show_help() {
-    printf("Usage: ./s21_cat [OPTION]... [FILE]...\n");
-    printf("Concatenate FILE(s) to standard output.\n\n");
-    
-    
-    printf("  -b, --number-nonblank    number nonempty output lines, overrides -n\n");
-    printf("  -e                       equivalent to -vE\n");
-    printf("  -E, --show-ends          display $ at end of each line\n");
-    printf("  -n, --number             number all output lines\n");
-    printf("  -s, --squeeze-blank      suppress repeated empty output lines\n");
-    printf("  -t                       equivalent to -vT\n");
-    printf("  -T, --show-tabs          display TAB characters as ^I\n");
-    printf("  -v, --show-nonprinting   use ^ and M- notation, except for LFD and TAB\n");
-    printf("      --help               display this help and exit\n\n");
-    
-    printf("Examples:\n");
-    printf("  s21_cat f - g  Output f's contents, then standard input, then g's contents.\n");
-    printf("  s21_cat        Copy standard input to standard output.\n\n\n");
-    
-    printf("cat implementation for School 21.\n\n");
-    printf("With love,\n");
-    printf("merlewhi/remagss\n");
+  printf("Usage: ./s21_cat [OPTION]... [FILE]...\n");
+  printf("Concatenate FILE(s) to standard output.\n\n");
+
+  printf(
+      "  -b, --number-nonblank    number nonempty output lines, overrides "
+      "-n\n");
+  printf("  -e                       equivalent to -vE\n");
+  printf("  -E, --show-ends          display $ at end of each line\n");
+  printf("  -n, --number             number all output lines\n");
+  printf("  -s, --squeeze-blank      suppress repeated empty output lines\n");
+  printf("  -t                       equivalent to -vT\n");
+  printf("  -T, --show-tabs          display TAB characters as ^I\n");
+  printf(
+      "  -v, --show-nonprinting   use ^ and M- notation, except for LFD and "
+      "TAB\n");
+  printf("      --help               display this help and exit\n\n");
+
+  printf("Examples:\n");
+  printf(
+      "  s21_cat f - g  Output f's contents, then standard input, then g's "
+      "contents.\n");
+  printf("  s21_cat        Copy standard input to standard output.\n\n\n");
+
+  printf("cat implementation for School 21.\n\n");
+  printf("With love,\n");
+  printf("merlewhi/remagss\n");
 }
--- src/cat/s21_cat_flags.c
+++ src/cat/s21_cat_flags.c
@@ -1,90 +1,94 @@
 #include "s21_cat_flags.h"
 
 void process_file_with_flags(FILE *file, cat_flags *flags) {
-    int ch;
-    int line_number = 1;
-    int prev_ch = '\n';
-    int empty_line_count = 0;
-    
-    while ((ch = fgetc(file)) != EOF) {
-        int result = process_character(ch, flags, &line_number, &prev_ch, &empty_line_count);
-        if (result != -1) {
-            putchar(result);
-        }
+  int ch;
+  int line_number = 1;
+  int prev_ch = '\n';
+  int empty_line_count = 0;
+
+  while ((ch = fgetc(file)) != EOF) {
+    int result =
+        process_character(ch, flags, &line_number, &prev_ch, &empty_line_count);
+    if (result != -1) {
+      putchar(result);
     }
+  }
 }
 
-int process_character(int ch, cat_flags *flags, int *line_number, int *prev_ch, int *empty_line_count) {
-    int should_print = 1;
-    int processed_char = ch;
-    
-    handle_numbering(flags, ch, line_number, prev_ch);
-    
-    should_print = handle_squeeze_blank(flags, ch, prev_ch, empty_line_count);
-    
-    if (should_print) {
-        processed_char = handle_nonprinting(ch, flags);
-        if (processed_char == -1) {
-            should_print = 0;
-        }
+int process_character(int ch, cat_flags *flags, int *line_number, int *prev_ch,
+                      int *empty_line_count) {
+  int should_print = 1;
+  int processed_char = ch;
+
+  handle_numbering(flags, ch, line_number, prev_ch);
+
+  should_print = handle_squeeze_blank(flags, ch, prev_ch, empty_line_count);
+
+  if (should_print) {
+    processed_char = handle_nonprinting(ch, flags);
+    if (processed_char == -1) {
+      should_print = 0;
     }
-    
-    *prev_ch = ch;
-    return should_print ? processed_char : -1;
+  }
+
+  *prev_ch = ch;
+  return should_print ? processed_char : -1;
 }
 
-void handle_numbering(cat_flags *flags, int ch, int *line_number, int *prev_ch) {
-    if (flags->number && *prev_ch == '\n') {
-        printf("%6d\t", (*line_number)++);
-    }
-    
-    if (flags->number_nonblank && *prev_ch == '\n' && ch != '\n') {
-        printf("%6d\t", (*line_number)++);
-    }
+void handle_numbering(cat_flags *flags, int ch, int *line_number,
+                      int *prev_ch) {
+  if (flags->number && *prev_ch == '\n') {
+    printf("%6d\t", (*line_number)++);
+  }
+
+  if (flags->number_nonblank && *prev_ch == '\n' && ch != '\n') {
+    printf("%6d\t", (*line_number)++);
+  }
 }
 
-int handle_squeeze_blank(cat_flags *flags, int ch, int *prev_ch, int *empty_line_count) {
-    int should_print = 1;
-    
-    if (flags->squeeze_blank) {
-        if (ch == '\n' && *prev_ch == '\n') {
-            (*empty_line_count)++;
-            if (*empty_line_count > 1) {
-                should_print = 0;
-            }
-        } else {
-            *empty_line_count = 0;
-        }
+int handle_squeeze_blank(cat_flags *flags, int ch, int *prev_ch,
+                         int *empty_line_count) {
+  int should_print = 1;
+
+  if (flags->squeeze_blank) {
+    if (ch == '\n' && *prev_ch == '\n') {
+      (*empty_line_count)++;
+      if (*empty_line_count > 1) {
+        should_print = 0;
+      }
+    } else {
+      *empty_line_count = 0;
     }
-    
-    return should_print;
+  }
+
+  return should_print;
 }
 
 int handle_nonprinting(int ch, cat_flags *flags) {
-    if (ch == EOF) {
-        return ch;
-    }
-    
-    unsigned char uc = (unsigned char)ch;
-    
-    if (uc == '\n' && flags->show_ends) {
-        printf("$");
-    }
-    
-    if (uc == '\t' && flags->show_tabs) {
-        printf("^I");
-        return -1;
-    }
-    
-    if (flags->show_nonprinting) {
-        if (uc < 32 && uc != 9 && uc != 10) {
-            printf("^%c", uc + 64);
-            return -1;
-        } else if (uc == 127) {
-            printf("^?");
-            return -1;
-        }
-    }
-    
+  if (ch == EOF) {
     return ch;
+  }
+
+  unsigned char uc = (unsigned char)ch;
+
+  if (uc == '\n' && flags->show_ends) {
+    printf("$");
+  }
+
+  if (uc == '\t' && flags->show_tabs) {
+    printf("^I");
+    return -1;
+  }
+
+  if (flags->show_nonprinting) {
+    if (uc < 32 && uc != 9 && uc != 10) {
+      printf("^%c", uc + 64);
+      return -1;
+    } else if (uc == 127) {
+      printf("^?");
+      return -1;
+    }
+  }
+
+  return ch;
 }
