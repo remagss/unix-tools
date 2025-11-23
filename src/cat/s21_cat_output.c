516a684e8e2033fce458a53eecd9fb271055f9bd
remagss <slprtzd@gmail.com>
2025-11-23 23:38:45 +0300

    feat: add new function process_character()

--- src/cat/s21_cat_output.c
+++ src/cat/s21_cat_output.c
@@ -13,3 +13,19 @@ void process_file_with_flags(FILE *file, cat_flags *flags) {
         }
     }
 }
+
+int process_character(int ch, cat_flags *flags, int *line_number, int *prev_ch, int *empty_line_count) {
+    int should_print = 1;
+    
+    handle_numbering(flags, ch, line_number, prev_ch);
+    
+    should_print = handle_squeeze_blank(flags, ch, prev_ch, empty_line_count);
+    
+    if (should_print) {
+        ch = handle_nonprinting(ch, flags);
+    }
+    
+    *prev_ch = ch;
+    return should_print;
+}
+
