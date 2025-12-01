a5082fdcb9044df0be2f73a01072f4b9b946de94
remagss <slprtzd@gmail.com>
2025-12-01 22:10:59 +0300

    feat: add heading of parse_flags()

--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -1,10 +1,10 @@
 #ifndef S21_GREP_H
 #define S21_GREP_H
 
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <regex.h>
 
 #define SUCCESS 0
 #define ERROR 1
@@ -12,21 +12,21 @@
 #define ERROR_FILE_OPEN 3
 
 typedef struct {
-    int e;  // pattern (-e)
-    int i;  // ignore uppercase vs lowercase (-i)
-    int v;  // invert match (-v)
-    int c;  // output count of matching lines only (-c)
-    int l;  // output matching files only (-l)
-    int n;  // precede each matching line with a line number (-n)
-    // int h;  // output matching lines without preceding them by file names (-h)
-    // int s;  // suppress error messages about nonexistent or unreadable files (-s)
-    // int f;  // take regexes from a file (-f [file])
-    // int o;  // output the matched parts of a matching line (-o)
-    int show_help;  // --help
+  int e;  // pattern (-e)
+  int i;  // ignore uppercase vs lowercase (-i)
+  int v;  // invert match (-v)
+  int c;  // output count of matching lines only (-c)
+  int l;  // output matching files only (-l)
+  int n;  // precede each matching line with a line number (-n)
+  // int h;  // output matching lines without preceding them by file names (-h)
+  // int s;  // suppress error messages about nonexistent or unreadable files
+  // (-s) int f;  // take regexes from a file (-f [file]) int o;  // output the
+  // matched parts of a matching line (-o)
+  int show_help;  // --help
 } grep_flags;
 
 int run_grep(int argc, char *argv[], grep_flags *flags);
+int parse_flags(int argc, char *argv[], grep_flags *flags, char **pattern);
 void handle_error(int error_code, char *argv[]);
-
 
 #endif
