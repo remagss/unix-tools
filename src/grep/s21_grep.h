353ef77b73cf844994678a9e06f8ef48761383b5
remagss <slprtzd@gmail.com>
2025-12-01 20:03:11 +0300

    feat: add basic struct of grep flags

--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -6,4 +6,17 @@
 #include <string.h>
 #include <regex.h>
 
+typedef struct {
+    int e;  // pattern (-e)
+    int i;  // ignore uppercase vs lowercase (-i)
+    int v;  // invert match (-v)
+    int c;  // output count of matching lines only (-c)
+    int l;  // output matching files only (-l)
+    int n;  // precede each matching line with a line number (-n)
+    // int h;  // output matching lines without preceding them by file names (-h)
+    // int s;  // suppress error messages about nonexistent or unreadable files (-s)
+    // int f;  // take regexes from a file (-f [file])
+    // int o;  // output the matched parts of a matching line (-o)
+} grep_flags;
+
 #endif
